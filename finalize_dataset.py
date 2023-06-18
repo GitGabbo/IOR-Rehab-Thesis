1737168f39c0c8645e68f09114ec4f6366c689fe
Gabriele Evangelista <gabryx28@hotmail.it>
2023-06-18 17:31:31 +0200

    Fixed finalization of dataset lengths

--- finalize_dataset.py
+++ finalize_dataset.py
@@ -8,7 +8,7 @@ import shutil
 def finalize_exercise(ex_number):
     VIDEO_CSV_DIR = f"videos_data/ex{ex_number}/"
     TARGET_CSV_DIR = f"targets_data/ex{ex_number}/"
-    FINAL_CSV_DIR = f"final_video_data/ex{ex_number}/"
+    FINAL_VIDEO_DIR = f"final_video_data/ex{ex_number}/"
     FINAL_TARGETS_DIR = f"final_targets_data/ex{ex_number}/"
 
     TARGET_CSV_FILES = os.listdir(TARGET_CSV_DIR)
@@ -19,20 +19,21 @@ def finalize_exercise(ex_number):
         name_initial = file[:6]
 
         with open(TARGET_CSV_DIR+file, "r") as score_f:
-            targets_lines = score_f.readlines()
-            number_reps = len(targets_lines) - 2
+            targets_lines = score_f.readlines()[1:-1]
+            number_reps = len(targets_lines)
 
             for rep in range(number_reps):
                 # video name (e.g. "001 SJ" + "1.1" + ".1" + ".csv")
                 video_name_file = f"{name_initial}.{file[10:13]}.{rep+1}.csv"
                 # open video file and merged file
                 try:
-                    video_adjusted_data = np.array(extracted_data_reader.read_data_video(f"{VIDEO_CSV_DIR}{video_name_file}", int(ex_number))).T
-                    pd.DataFrame(video_adjusted_data).to_csv(f"{FINAL_CSV_DIR}{video_name_file}", header=extracted_data_reader.LABELS, index=None)
+                    shutil.copy2(f"{VIDEO_CSV_DIR}{video_name_file}", f"{FINAL_VIDEO_DIR}{video_name_file}")
                 except FileNotFoundError:
                     print(f"Video file {video_name_file} does not exist, skipping its valutation file")
+                    TARGET_CSV_FILES.remove(file)
                 except ZeroDivisionError:
                     print(f"Video file {video_name_file} could not be analyzed by mediapipe. Skipping repetition and its valutation")
+                    TARGET_CSV_FILES.remove(file)
 
     for file in TARGET_CSV_FILES:
         shutil.copy2(f"{TARGET_CSV_DIR}{file}", f"{FINAL_TARGETS_DIR}{file}")
